2cf3dcd4db3728fe6b80a76048eede0d2276871d
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-01-09 10:23:48 -0600

    Getting new employee data from form

--- app2.js
+++ app2.js
@@ -4,4 +4,24 @@
 
 $(function() {
   console.log('jQuery is working!!')
-})
+  $('#newEmployeeForm').on('submit', function(event) {
+    // console.log(event);
+    event.preventDefault(); //stop the page from reloading and redirectiong
+    console.log('Form has been submitted!!');
+
+    var newEmployeeArray = $(this).serializeArray(); //get the information from the form
+    //console.log(newEmployee);
+
+    var newEmployeeObject = {};
+
+    // Loop through all of the input properties in the array
+    for(var i = 0; i < newEmployeeArray.length; i ++) {
+      // console.log(newEmployeeArray[i]);
+      var inputFieldName = newEmployeeArray[i].name;
+      var inputFieldValue = newEmployeeArray[i].value;
+      newEmployeeObject[inputFieldName] = inputFieldValue;
+    }
+    // Do not need semi-colon after "for" loops. Only for assignments and functions.
+    console.log(newEmployeeObject);
+  });
+});
